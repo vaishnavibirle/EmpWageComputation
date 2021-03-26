59495b8a0185122f959c6685b82a416fca180e92
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-26 23:17:59 +0530

    [vaishnavi] adding EmpWageBuilder

---
+++ EmpWageBuilder.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class EmpWageBuilder {
+
+    public static void main(String[] args) {
+	// welcome message
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
