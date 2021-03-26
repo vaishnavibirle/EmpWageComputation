7e315dc9721d32493271a4765ef3372b98582ada
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-26 23:34:19 +0530

    adding UC2

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,15 +1,19 @@
 package com.company;
 
-public class EmpWageBuilder {
+public class EmpWageBuilderUC2 {
     public static void main(String[] args)
     {
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
+            empHrs = 8;
         else
-            System.out.println("Employee is absent");
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
     }
 }
-
 
