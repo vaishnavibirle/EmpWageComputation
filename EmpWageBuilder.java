675af8b15409fb39a4aa7b3b86e4efb9627024f9
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-26 23:43:39 +0530

    adding UC4

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,21 +1,26 @@
 package com.company;
 
-public class EmpWageBuilderUC3 {
+public class EmpWageBuilder {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
     public static void main(String[] args)
     {
-        //constants
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-        //variables
         int empHrs = 0;
         int empWage = 0;
-        //computation
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME)
-            empHrs = 6;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 8;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck)
+        {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default :
+                empHrs = 0;
+        }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
     }
