1ce360f40833a5188e57af4d5274e03245146e9f
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-26 23:30:11 +0530

    adding UC1

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,9 +1,15 @@
 package com.company;
 
 public class EmpWageBuilder {
-
-    public static void main(String[] args) {
-	// welcome message
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    public static void main(String[] args)
+    {
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
+
+
